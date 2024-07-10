3a4cc1a849f253fa24e2f42af19ca5876ce56a45
palomacalvin <calvinpaloma@gmail.com>
2024-07-09 20:24:40 -0400

    Paloma plot

--- practice-1.py
+++ practice-1.py
@@ -3,8 +3,9 @@ import numpy as np
 import pandas
 
 csv_file = pandas.read_csv("data.csv")
-# paloma = csv_file["Paloma Score"].values.tolist()
+paloma = csv_file["Paloma Score"].values.tolist()
+episodes = range(len(paloma))
 
 fig, ax = plt.subplots()             # Create a figure containing a single Axes.
-ax.plot(csv_file)                    # Plot some data on the Axes.
+ax.scatter(episodes, paloma)                    # Plot some data on the Axes.
 plt.show()                           # Show the figure.
